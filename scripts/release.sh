28e82e051cb83dd68a06ce4fd5cfa832a6d4032d
Rijnard van Tonder <rvantonder@gmail.com>
2019-04-29 00:22:32 -0400

    release script update

--- scripts/release.sh
+++ scripts/release.sh
@@ -14,7 +14,7 @@ make release
 make test
 
 cp _build/default/src/main.exe scripts/$VERSION/comby-$VERSION-x86_64-macos
-cp scripts/check-and-install.sh scripts/$VERSION/0.sh
-cp scripts/install-with-licenses.sh scripts/$VERSION/get.sh
-comby '"0.x.0"' "$VERSION" * -i .sh -d scripts/$VERSION
+cp scripts/check-and-install.sh scripts/$VERSION-0/index.html
+cp scripts/install-with-licenses.sh scripts/$VERSION-get/index.html
+comby '"0.x.0"' "$VERSION" * -i .html -d scripts/$VERSION
 echo "Remember to binaries for linux."
