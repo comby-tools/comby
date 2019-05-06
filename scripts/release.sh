4e8d195f90d1501975dd9fa72d1d77725078cb73
Rijnard van Tonder <rvantonder@gmail.com>
2019-05-06 02:35:38 -0400

    release script fixup

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,9 +2,9 @@
 
 VERSION="0.x.0"
 
-if [ -z "$1" ]
-  then
+if [ -z "$1" ]; then
     echo "Need arg: what version to release?"
+    exit 1
 fi
 
 VERSION=$1
@@ -37,4 +37,3 @@ fi
 cp scripts/check-and-install.sh scripts/$VERSION/0/index.html
 cp scripts/install-with-licenses.sh scripts/$VERSION/get/index.html
 comby '"0.x.0"' "$VERSION" * -i .html -d scripts/$VERSION
-echo "Remember to binaries for linux."
