408f32b817808f1268315c65713bca5601d25a16
Rijnard van Tonder <rvantonder@gmail.com>
2019-06-06 15:55:17 -0400

    delete useless delimiter stuff

--- lib/parsers/delimiters.ml
+++ lib/parsers/delimiters.ml
@@ -1,26 +1,4 @@
-open Core
 open MParser
-
-(** Significant, potentially nested delimiters. *)
-
-let parens' p =
-  char '(' >> p << char ')'
-
-let braces' p =
-  char '{' >> p << char '}'
-
-let brackets' p =
-  char '<' >> p << char '>'
-
-let squares' p =
-  char '[' >> p << char ']'
-
-let between_ambiguous p (from : string list) until =
-  (List.map from ~f:string |> choice)
-  >> p << string until
-
-let between_p p pp until =
-  pp >> p << string until
 
 let between p from until =
   string from >> p << string until
