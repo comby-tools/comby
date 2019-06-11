db51e0a1f94e4dfaa18f5868c8a3f76834373133
Rijnard van Tonder <rvantonder@gmail.com>
2019-06-11 03:26:36 -0400

    remove cli test flakiness

--- test/test_cli.ml
+++ test/test_cli.ml
@@ -11,7 +11,7 @@ let read_with_timeout read_from_channels =
       ~read:read_from_fds
       ~write:[]
       ~except:[]
-      ~timeout:(`After (Time.of_int_sec 5))
+      ~timeout:(`After (Time.of_int_sec 1))
       ()
     |> (fun { Unix.Select_fds.read; _ } -> List.hd_exn read)
     |> Unix.in_channel_of_descr
@@ -43,8 +43,16 @@ let%expect_test "json_lines_separates_by_line" =
     Format.sprintf "-stdin -sequential '%s' '%s' -f .c -json-lines" match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|{"uri":null,"rewritten_source":"helli wirld","in_place_substitutions":[{"range":{"start":{"offset":7,"line":-1,"column":-1},"end":{"offset":8,"line":-1,"column":-1}},"replacement_content":"i","environment":[]},{"range":{"start":{"offset":4,"line":-1,"column":-1},"end":{"offset":5,"line":-1,"column":-1}},"replacement_content":"i","environment":[]}],"diff":"--- /dev/null\n+++ /dev/null\n@@ -1,1 +1,1 @@\n-hello world\n+helli wirld"}
 |}]
 
@@ -56,8 +64,16 @@ let%expect_test "json_lines_json_pretty_do_not_output_when_diff_null" =
     Format.sprintf "-stdin -sequential '%s' '%s' -f .c -json-pretty" match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{||}]
 
 let%expect_test "json_lines_do_not_output_when_diff_null" =
@@ -68,15 +84,31 @@ let%expect_test "json_lines_do_not_output_when_diff_null" =
     Format.sprintf "-stdin -sequential '%s' '%s' -f .c -json-lines" match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{||}]
 
 let%expect_test "error_on_zip_and_stdin" =
   let command_args = "-zip x -stdin" in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command "none"
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command "none"
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|No templates specified. Either on the command line, or using -templates <directory-containing-templates>
 Next error: -zip may not be used with stdin.
 |}]
@@ -89,8 +121,16 @@ let%expect_test "warn_on_anonymous_and_templates_flag" =
     Format.sprintf "-stdin -sequential '%s' '%s' -f .c -templates nonexistent" match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|Warning: Templates specified on the command line AND using -templates. Ignoring match
       and rewrite templates on the command line and only using those in directories.
 Could not read required match file nonexistent/match
@@ -104,8 +144,16 @@ let%expect_test "warn_json_lines_and_json_pretty" =
     Format.sprintf "-stdin -sequential '%s' '%s' -f .c -json-lines -json-pretty" match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|Warning: Both -json-lines and -json-pretty specified. Using -json-pretty.
 |}]
 
@@ -117,8 +165,16 @@ let%expect_test "stdin_command" =
     Format.sprintf "-stdin -sequential '%s' '%s' -f .c" match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|[0;31m------ [0m[0;1m/dev/null[0m
 [0;32m++++++ [0m[0;1m/dev/null[0m
 [0;100;30m@|[0m[0;1m-1,1 +1,1[0m ============================================================
@@ -135,8 +191,16 @@ let%expect_test "with_match_rule" =
       match_template rewrite_template rule
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|[0;31m------ [0m[0;1m/dev/null[0m
 [0;32m++++++ [0m[0;1m/dev/null[0m
 [0;100;30m@|[0m[0;1m-1,1 +1,1[0m ============================================================
@@ -152,8 +216,16 @@ let%expect_test "with_match_rule" =
       match_template rewrite_template rule
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{| hello world |}]
 
 let%expect_test "with_rewrite_rule" =
@@ -166,8 +238,16 @@ let%expect_test "with_rewrite_rule" =
       match_template rewrite_template rule
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|[0;31m------ [0m[0;1m/dev/null[0m
 [0;32m++++++ [0m[0;1m/dev/null[0m
 [0;100;30m@|[0m[0;1m-1,1 +1,1[0m ============================================================
@@ -183,8 +263,16 @@ let%expect_test "with_rewrite_rule_stdin_default_no_extension" =
     Format.sprintf "-sequential '%s' '%s' -rule '%s' -stdin" match_template rewrite_template rule
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|[0;31m------ [0m[0;1m/dev/null[0m
 [0;32m++++++ [0m[0;1m/dev/null[0m
 [0;100;30m@|[0m[0;1m-1,1 +1,1[0m ============================================================
@@ -199,8 +287,16 @@ let%expect_test "generic_matcher" =
     Format.sprintf "-stdin -sequential '%s' '%s' -f .generic" match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|[0;31m------ [0m[0;1m/dev/null[0m
 [0;32m++++++ [0m[0;1m/dev/null[0m
 [0;100;30m@|[0m[0;1m-1,1 +1,1[0m ============================================================
@@ -218,8 +314,16 @@ let%expect_test "json_output_option" =
       match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|{
   "uri": null,
   "rewritten_source": "c X a c Y a",
@@ -272,8 +376,16 @@ let%expect_test "json_output_option" =
       match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|{
   "uri": null,
   "matches": [
@@ -333,8 +445,16 @@ let%expect_test "patdiff_and_zip" =
           match_template rewrite_template file
       in
       let command = Format.sprintf "%s %s" binary_path command_args in
-      read_output command
-      |> print_string;
+      let result =
+        let rec rerun () =
+          try
+            read_output command
+          with
+          | Unix.Unix_error (EINTR, _, _) -> rerun ()
+        in
+        rerun ()
+      in
+      print_string result;
       [%expect_exact {|{
   "uri": "main.ml",
   "rewritten_source": "world",
@@ -367,8 +487,16 @@ let%expect_test "template_parsing_no_match_template" =
   let template_dir = "example" ^/ "templates" ^/ "parse-no-match-template" in
   let command_args = Format.sprintf "-stdin -sequential -f .c -templates %s" template_dir in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|Could not read required match file example/templates/parse-no-match-template/match
 |}]
 
@@ -377,8 +505,16 @@ let%expect_test "template_parsing_with_trailing_newline" =
   let template_dir = "example" ^/ "templates" ^/ "parse-template-no-trailing-newline" in
   let command_args = Format.sprintf "-stdin -sequential -f .c -templates %s -stdout" template_dir in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{| hello world |}]
 
 let%expect_test "template_parsing_with_trailing_newline" =
@@ -386,8 +522,16 @@ let%expect_test "template_parsing_with_trailing_newline" =
   let template_dir = "example" ^/ "templates" ^/ "parse-template-with-trailing-newline" in
   let command_args = Format.sprintf "-stdin -sequential -f .c -templates %s -stdout" template_dir in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{| hello world |}]
 
 let%expect_test "diff_is_default" =
@@ -399,8 +543,16 @@ let%expect_test "diff_is_default" =
       match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|[0;31m------ [0m[0;1m/dev/null[0m
 [0;32m++++++ [0m[0;1m/dev/null[0m
 [0;100;30m@|[0m[0;1m-1,1 +1,1[0m ============================================================
@@ -417,8 +569,16 @@ let%expect_test "diff_option" =
       match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|--- /dev/null
 +++ /dev/null
 @@ -1,1 +1,1 @@
@@ -448,8 +608,16 @@ let%expect_test "only_color_prints_colored_diff" =
       match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|[0;31m------ [0m[0;1m/dev/null[0m
 [0;32m++++++ [0m[0;1m/dev/null[0m
 [0;100;30m@|[0m[0;1m-1,1 +1,1[0m ============================================================
@@ -466,8 +634,16 @@ let%expect_test "diff_explicit_color" =
       match_template rewrite_template
   in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect_exact {|[0;31m------ [0m[0;1m/dev/null[0m
 [0;32m++++++ [0m[0;1m/dev/null[0m
 [0;100;30m@|[0m[0;1m-1,1 +1,1[0m ============================================================
@@ -480,8 +656,16 @@ let%expect_test "is_real_directory" =
   let src_dir = "example" ^/ "src" ^/ "main.c" in
   let command_args = Format.sprintf "'main' 'pain' -sequential -d %s -exclude-dir 'ignore' -diff" src_dir in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{|
     Directory specified with -d or -r or -directory is not a directory |}]
 
@@ -490,8 +674,16 @@ let%expect_test "exclude_dir_option" =
   let src_dir = "example" ^/ "src" in
   let command_args = Format.sprintf "'main' 'pain' -sequential -d %s -exclude-dir 'ignore' -diff" src_dir in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{|
     --- example/src/main.c
     +++ example/src/main.c
@@ -502,8 +694,16 @@ let%expect_test "exclude_dir_option" =
   let src_dir = "example" ^/ "src" in
   let command_args = Format.sprintf "'main' 'pain' -sequential -d %s -exclude-dir 'nonexist' -diff" src_dir in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{|
     --- example/src/ignore-me/main.c
     +++ example/src/ignore-me/main.c
@@ -521,17 +721,32 @@ let%expect_test "dir_depth_option" =
   let src_dir = "example" ^/ "src" in
   let command_args = Format.sprintf "'depth_' 'correct_depth_' -sequential -directory %s -depth %d -diff" src_dir (-1) in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{| -depth must be 0 or greater |}];
-
 
   let source = "hello world" in
   let src_dir = "example" ^/ "src" in
   let command_args = Format.sprintf "'depth_' 'correct_depth_' -sequential -directory %s -depth %d -diff" src_dir 0 in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{|
     --- example/src/depth-0.c
     +++ example/src/depth-0.c
@@ -543,8 +758,16 @@ let%expect_test "dir_depth_option" =
   let src_dir = "example" ^/ "src" in
   let command_args = Format.sprintf "'depth_' 'correct_depth_' -sequential -directory %s -depth %d -diff" src_dir 1 in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{|
     --- example/src/depth-0.c
     +++ example/src/depth-0.c
@@ -561,8 +784,16 @@ let%expect_test "dir_depth_option" =
   let src_dir = "example" ^/ "src" in
   let command_args = Format.sprintf "'depth_' 'correct_depth_' -sequential -directory %s -depth %d -diff" src_dir 2 in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{|
     --- example/src/depth-0.c
     +++ example/src/depth-0.c
@@ -584,8 +815,16 @@ let%expect_test "dir_depth_option" =
   let src_dir = "example" ^/ "src" in
   let command_args = Format.sprintf "'depth_' 'correct_depth_' -sequential -directory %s -depth %d -diff" src_dir 1000 in
   let command = Format.sprintf "%s %s" binary_path command_args in
-  read_source_from_stdin command source
-  |> print_string;
+  let result =
+    let rec rerun () =
+      try
+        read_source_from_stdin command source
+      with
+      | Unix.Unix_error (EINTR, _, _) -> rerun ()
+    in
+    rerun ()
+  in
+  print_string result;
   [%expect{|
     --- example/src/depth-0.c
     +++ example/src/depth-0.c
