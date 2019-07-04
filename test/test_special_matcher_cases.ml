02cd9f0ebe0625e6ae1fe77891b42f0ffc4867ad
Rijnard van Tonder <rvantonder@gmail.com>
2019-07-03 20:13:24 -0400

    add tests

--- test/test_special_matcher_cases.ml
+++ test/test_special_matcher_cases.ml
@@ -150,3 +150,246 @@ let%expect_test "ruby_blocks" =
 
   run (module Matchers.Ruby) source match_template rewrite_template;
   [%expect_exact {|<1>x</1>|}]
+
+let%expect_test "ruby_blocks_1" =
+  let source = {|class class def body1 end def body2 end end end|} in
+  let match_template = {|class :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>class def body1 end def body2 end end</1>|}]
+
+let%expect_test "ruby_blocks_2" =
+  let source = {|class class (def body1 end) (def body2 end) end end|} in
+  let match_template = {|class :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>class (def body1 end) (def body2 end) end</1>|}]
+
+let%expect_test "ruby_blocks_3" =
+  let source = {| def (def b end)(def b end) end |} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {| <1>(def b end)(def b end)</1> |}]
+
+let%expect_test "ruby_blocks_4" =
+  let source = {| def (def a end) f (def b end)end |} in
+  let match_template = {|def :[1]end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {| <1>(def a end) f (def b end)</1> |}]
+
+(* this is correct: there's no space in the source 'end', so it matches the inner def/end blocks *)
+let%expect_test "ruby_blocks_5" =
+  let source = {| def (def a end) f (def b end) end |} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {| <1>(def a end) f (def b end)</1> |}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {| def(df b ed) (df b ed)end |} in
+  let match_template = {|def:[1]end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {| <1>(df b ed) (df b ed)</1> |}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|class class ((def (x) end) f (def body end)) end end|} in
+  let match_template = {|class :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>class ((def (x) end) f (def body end)) end</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|class class (def body1 end) (def body2 end) end end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|class class (<1>body1</1>) (<1>body2</1>) end end|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|class class (def body1 end) (def body2 end) end end|} in
+  let match_template = {|class :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>class (def body1 end) (def body2 end) end</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|class class def (def body1 end) (def body2 end) end end end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|class class <1>(def body1 end) (def body2 end)</1> end end|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|class class def () (def body2 end) end end end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|class class <1>() (def body2 end)</1> end end|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def (def end) (def end) end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>(def end) (def end)</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def (def end) a (def end) end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>(def end) a (def end)</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def (defa aend) (adef aend) end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>(defa aend) (adef aend)</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def (defa aend) a (adef aend) end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>(defa aend) a (adef aend)</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def (adef a endq) end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>(adef a endq)</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def adef a endq end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>adef a endq</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def def foo end end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>def foo end</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def def end endq|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|No matches.|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def adef a endq end |} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>adef a endq</1> |}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def fadef a qendq end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>fadef a qendq</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def defa aend end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>defa aend</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|(adef a endq)|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|No matches.|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def adef a endq end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>adef a endq</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def (adef a endq) end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>(adef a endq)</1>|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|class (def ( body )end) end|} in
+  let match_template = {|class :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|<1>(def ( body )end)</1>|}]
+
+(* FIX *)
+let%expect_test "ruby_blocks_5" =
+  let source = {|class ( def( body )end) end|} in
+  let match_template = {|class :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|No matches.|}]
+
+(* FIX *)
+let%expect_test "ruby_blocks_5" =
+  let source = {|class (def( body )end) end|} in
+  let match_template = {|class :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|No matches.|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|(def a endq)|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|No matches.|}]
+
+let%expect_test "ruby_blocks_5" =
+  let source = {|def  end|} in
+  let match_template = {|def :[1] end|} in
+  let rewrite_template = {|<1>:[1]</1>|} in
+
+  run (module Matchers.Ruby) source match_template rewrite_template;
+  [%expect_exact {|No matches.|}]
