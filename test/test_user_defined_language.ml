a9d3332b88346b857f4dc76080362fc681d466be
Thorsten Ball <mrnugget@gmail.com>
2019-07-30 11:41:23 +0200

    Add test for user defined language

--- test/test_user_defined_language.ml
+++ test/test_user_defined_language.ml
@@ -1,16 +1,63 @@
 open Core
 open Matchers
+open Rewriter
 
-let%expect_test "serializing user defined language" =
+let configuration = Configuration.create ~match_kind:Fuzzy ()
+
+let run lang source match_template rewrite_template =
+  let module User_lang = (val lang : Matchers.Matcher) in
+  User_lang.first ~configuration match_template source
+  |> function
+  | Ok result ->
+      Rewrite.all ~source ~rewrite_template [result]
+      |> (fun x -> Option.value_exn x)
+      |> (fun {rewritten_source; _} -> rewritten_source)
+      |> print_string
+  | Error _ ->
+      print_string rewrite_template
+
+let%expect_test "user_defined_language" =
   let c =
-    { Syntax_config.user_defined_delimiters= [("if", "fi")]
+    { Syntax_config.user_defined_delimiters= [("case", "esac")]
     ; Syntax_config.escapable_string_literals= []
     ; Syntax_config.escape_char= '\\'
-    ; Syntax_config.raw_string_literals= [({|"|}, {|"|})]
-    ; Syntax_config.comment_parser= [Until_newline "%"] }
+    ; Syntax_config.raw_string_literals= []
+    ; Syntax_config.comment_parser= [Multiline ("/*", "*/"); Until_newline "//"]
+    }
   in
-  Matchers.Syntax_config.to_yojson c |> Yojson.Safe.to_string |> print_string ;
-  [%expect
+  let user_lang = Matchers.create c in
+  let source =
     {|
-    {"user_defined_delimiters":[["if","fi"]],"escapable_string_literals":[],"escape_char":"\\","raw_string_literals":[["\"","\""]],"comment_parser":[["Until_newline","%"]]}
-  |}]
+      case
+        case
+          block 1
+        esac
+
+        case
+          block 2
+        esac
+      esac
+      /*
+      case
+        ignore this
+      esac
+      */
+      // case
+      //   ignore this
+      // esac
+    |}
+  in
+  let match_template = {|case :[1] esac|} in
+  let rewrite_template = {|case nuked blocks esac|} in
+  run user_lang source match_template rewrite_template ;
+  [%expect_exact {|
+      case nuked blocks esac
+      /*
+      case
+        ignore this
+      esac
+      */
+      // case
+      //   ignore this
+      // esac
+    |}]
