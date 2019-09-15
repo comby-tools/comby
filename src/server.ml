bb82a461f7f88d6cd79aaccfa4cc11999bfcaa63
Rijnard van Tonder <rvantonder@gmail.com>
2019-09-15 16:26:23 -0400

    add substitution and refactor server types

--- src/server.ml
+++ src/server.ml
@@ -6,6 +6,8 @@ open Language
 open Matchers
 open Rewriter
 
+open Server_types
+
 let (>>|) = Lwt.Infix.(>|=)
 
 let debug =
@@ -17,44 +19,6 @@ let max_request_length =
   match Sys.getenv "MAX_REQUEST_LENGTH" with
   | None -> Int.max_value
   | Some max -> Int.of_string max
-
-type match_request =
-  { source : string
-  ; match_template : string [@key "match"]
-  ; rule : string option [@default None]
-  ; language : string [@default "generic"]
-  ; id : int
-  }
-[@@deriving yojson]
-
-type rewrite_request =
-  { source : string
-  ; match_template : string [@key "match"]
-  ; rewrite_template : string [@key "rewrite"]
-  ; rule : string option [@default None]
-  ; language : string [@default "generic"]
-  ; substitution_kind : string [@default "in_place"]
-  ; id : int
-  }
-[@@deriving yojson]
-
-type json_match_result =
-  { matches : Match.t list
-  ; source : string
-  ; id : int
-  }
-[@@deriving yojson]
-
-type json_rewrite_result =
-  { rewritten_source : string
-  ; in_place_substitutions : Replacement.t list
-  ; id : int
-  }
-[@@deriving yojson]
-
-let matches_to_json source id matches =
-  Format.sprintf "%s"
-    (Yojson.Safe.pretty_to_string (json_match_result_to_yojson { matches; source; id }))
 
 let check_too_long s =
   let n = String.length s in
@@ -69,21 +33,17 @@ let check_too_long s =
 
 let perform_match request =
   App.string_of_body_exn request
-  >>| (fun s ->
-      match check_too_long s with
-      | Ok s ->
-        Yojson.Safe.from_string s
-        |> match_request_of_yojson
-      | Error e -> Error e
-    )
+  >>| check_too_long
+  >>| Result.map ~f:(Fn.compose In.match_request_of_yojson Yojson.Safe.from_string)
+  >>| Result.join
   >>| function
   | Ok ({ source; match_template; rule; language; id } as request) ->
-    if debug then Format.printf "Received %s@." (Yojson.Safe.pretty_to_string (match_request_to_yojson request));
+    if debug then Format.printf "Received %s@." (Yojson.Safe.pretty_to_string (In.match_request_to_yojson request));
     let matcher = Matchers.select_with_extension language in
     let run ?rule () =
       let configuration = Configuration.create ~match_kind:Fuzzy () in
       Pipeline.run matcher ?rule configuration match_template source
-      |> matches_to_json source id
+      |> fun matches -> Out.Matches.to_string { matches; source; id }
     in
     let code, result =
       match Option.map rule ~f:Rule.create with
@@ -97,27 +57,14 @@ let perform_match request =
     if debug then Format.printf "Result (400) %s@." error;
     respond ~code:(`Code 400) (`String error)
 
-let rewrite_to_json id ({ rewritten_source; in_place_substitutions } : Replacement.result) =
-  Format.sprintf "%s"
-    (Yojson.Safe.pretty_to_string
-       (json_rewrite_result_to_yojson
-          { rewritten_source
-          ; in_place_substitutions
-          ; id
-          }))
-
 let perform_rewrite request =
   App.string_of_body_exn request
-  >>| (fun s ->
-      match check_too_long s with
-      | Ok s ->
-        Yojson.Safe.from_string s
-        |> rewrite_request_of_yojson
-      | Error e -> Error e
-    )
+  >>| check_too_long
+  >>| Result.map ~f:(Fn.compose In.rewrite_request_of_yojson Yojson.Safe.from_string)
+  >>| Result.join
   >>| function
   | Ok ({ source; match_template; rewrite_template; rule; language; substitution_kind; id } as request) ->
-    if debug then Format.printf "Received %s@." (Yojson.Safe.pretty_to_string (rewrite_request_to_yojson request));
+    if debug then Format.printf "Received %s@." (Yojson.Safe.pretty_to_string (In.rewrite_request_to_yojson request));
     let matcher = Matchers.select_with_extension language in
     let source_substitution, substitute_in_place =
       match substitution_kind with
@@ -125,18 +72,22 @@ let perform_rewrite request =
       | "in_place" | _ -> Some source, true
     in
     let default =
-      { rewritten_source = ""
-      ; in_place_substitutions = []
-      ; id
-      }
-      |> json_rewrite_result_to_yojson
-      |> Yojson.Safe.pretty_to_string
+      Out.Rewrite.to_string
+        { rewritten_source = ""
+        ; in_place_substitutions = []
+        ; id
+        }
     in
     let run ?rule () =
       let configuration = Configuration.create ~match_kind:Fuzzy () in
       Pipeline.run matcher ?rule ~substitute_in_place configuration match_template source
       |> Rewrite.all ?source:source_substitution ~rewrite_template
-      |> Option.value_map ~default ~f:(rewrite_to_json id)
+      |> Option.value_map ~default ~f:(fun Replacement.{ rewritten_source; in_place_substitutions } ->
+          Out.Rewrite.to_string
+            { rewritten_source
+            ; in_place_substitutions
+            ; id
+            })
     in
     let code, result =
       match Option.map rule ~f:Rule.create with
@@ -148,6 +99,26 @@ let perform_rewrite request =
     respond ~code:(`Code code) (`String result)
   | Error error ->
     if debug then Format.printf "Result (400): %s@." error;
+    respond ~code:(`Code 400) (`String error)
+
+let perform_environment_substitution request =
+  App.string_of_body_exn request
+  >>| Yojson.Safe.from_string
+  >>| In.substitution_request_of_yojson
+  >>| function
+  | Ok ({ rewrite_template; environment; id } as request) ->
+    if debug then Format.printf "Received %s@." (Yojson.Safe.pretty_to_string (In.substitution_request_to_yojson request));
+    let code, result =
+      200,
+      Out.Substitution.to_string
+        { result = fst @@ Rewrite_template.substitute rewrite_template environment
+        ; id
+        }
+    in
+    if debug then Format.printf "Result (%d) %s@." code result;
+    respond ~code:(`Code code) (`String result)
+  | Error error ->
+    if debug then Format.printf "Result (400) %s@." error;
     respond ~code:(`Code 400) (`String error)
 
 let add_cors_headers (headers: Cohttp.Header.t): Cohttp.Header.t =
@@ -180,5 +151,6 @@ let () =
   App.empty
   |> App.post "/match" perform_match
   |> App.post "/rewrite" perform_rewrite
+  |> App.post "/substitute" perform_environment_substitution
   |> App.middleware allow_cors
   |> App.run_command
