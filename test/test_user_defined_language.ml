f202e857fe84a915b0cbc20364bae87bdc29d3b1
Rijnard van Tonder <rvantonder@gmail.com>
2019-09-16 08:59:56 -0400

    add json parsing test for custom matcher

--- test/test_user_defined_language.ml
+++ test/test_user_defined_language.ml
@@ -18,14 +18,73 @@ let run (module M : Matchers.Matcher) source match_template rewrite_template =
 let%expect_test "user_defined_language" =
   let c =
     Syntax.
-      { user_defined_delimiters= [("case", "esac")]
-      ; escapable_string_literals= []
-      ; escape_char= '\\'
-      ; raw_string_literals= []
+      { user_defined_delimiters = [("case", "esac")]
+      ; escapable_string_literals = []
+      ; escape_char = '\\'
+      ; raw_string_literals = []
       ; comments = [Multiline ("/*", "*/"); Until_newline "//"]
       }
   in
   let user_lang = Matchers.create c in
+  let source =
+    {|
+      case
+        case
+          block 1
+        esac
+
+        case
+          block 2
+        esac
+      esac
+      /*
+      case
+        ignore this
+      esac
+      */
+      // case
+      //   ignore this
+      // esac
+    |}
+  in
+  let match_template = {|case :[1] esac|} in
+  let rewrite_template = {|case nuked blocks esac|} in
+  run user_lang source match_template rewrite_template ;
+  [%expect_exact {|
+      case nuked blocks esac
+      /*
+      case
+        ignore this
+      esac
+      */
+      // case
+      //   ignore this
+      // esac
+    |}]
+
+let%expect_test "user_defined_language_from_json" =
+  let json =
+    {|{
+      "user_defined_delimiters": [
+        ["case", "esac"]
+      ],
+      "escapable_string_literals": [
+      ],
+      "escape_char": "\\",
+      "raw_string_literals": [],
+      "comments": [
+        [ "Multiline", "/*", "*/" ],
+        [ "Until_newline", "//" ]
+      ]
+      }
+    |}
+  in
+  let user_lang =
+    Yojson.Safe.from_string json
+    |> Matchers.Syntax.of_yojson
+    |> Result.ok_or_failwith
+    |> Matchers.create
+  in
   let source =
     {|
       case
