218924137a5a83448a46bf37201e0279dc85778c
Rijnard van Tonder <rvantonder@gmail.com>
2019-09-20 01:14:13 -0400

    ok

--- lib/match/match.mli
+++ lib/match/match.mli
@@ -68,6 +68,8 @@ type t =
 
 val create : unit -> t
 
+val pp : Format.formatter -> string option * t list -> unit
+
 val pp_json_lines : Format.formatter -> string option * t list -> unit
 
 val pp_match_count : Format.formatter -> string option * t list -> unit
