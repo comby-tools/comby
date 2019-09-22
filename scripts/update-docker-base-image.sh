9c2ab94dd02e1c176d29184e5225e50573e952c5
Rijnard van Tonder <rvantonder@gmail.com>
2019-09-22 18:40:10 -0400

    add missing yojson

--- scripts/update-docker-base-image.sh
+++ scripts/update-docker-base-image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# docker rmi comby/base-dependencies-alpine-3.10:X.X.X
 cd ../dockerfiles/alpine/base-dependencies
-docker build -t comby/base-dependencies-alpine:master .
-docker push comby/base-dependencies-alpine:master
+docker build -t comby/base-dependencies-alpine-3.10:X.X.X .
+# docker push comby/base-dependencies-alpine-3.10:latest
