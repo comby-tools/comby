e8a7d9e625ba67385c79674f54398021866d76e5
Rijnard van Tonder <rvantonder@gmail.com>
2019-09-22 19:29:29 -0400

    Update opam and dependencies

--- push-coverage-report.sh
+++ push-coverage-report.sh
@@ -2,8 +2,8 @@
 
 bisect-ppx-report \
     -I _build/default/ \
-    -coveralls coverage.json \
-    -service-name travis-ci \
-    -service-job-id $TRAVIS_JOB_ID \
+    --coveralls coverage.json \
+    --service-name travis-ci \
+    --service-job-id $TRAVIS_JOB_ID \
     `find . -name 'bisect*.out'`
 curl -L -F json_file=@./coverage.json https://coveralls.io/api/v1/jobs
