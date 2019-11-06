06816fed386779d291a21f86c69665d31ca9d541
Rijnard van Tonder <rvantonder@gmail.com>
2019-11-06 00:10:00 -0700

    naming

--- scripts/build-docker-binary-releases.sh
+++ scripts/build-docker-binary-releases.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd ..
-docker build --tag comby-aline-binary-release -f dockerfiles/alpine/binary-release/Dockerfile .
+docker build --tag comby-alpine-binary-release -f dockerfiles/alpine/binary-release/Dockerfile .
