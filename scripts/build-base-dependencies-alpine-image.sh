1a6463ba3a6e4f3128b14a7d234df76cbfa0d737
Rijnard van Tonder <rvantonder@gmail.com>
2019-11-18 19:20:21 -0700

    correct tag for dependencies

--- scripts/build-base-dependencies-alpine-image.sh
+++ scripts/build-base-dependencies-alpine-image.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ..
-docker build --no-cache --tag comby/comby-base-dependencies-alpine-3.10 -f dockerfiles/alpine/base-dependencies/Dockerfile .
+docker build --no-cache --tag comby/base-dependencies-alpine-3.10 -f dockerfiles/alpine/base-dependencies/Dockerfile .
 docker push comby/base-dependencies-alpine-3.10:latest
+echo "Now run ./build-docker-binary-releases.sh to make sure the base dependencies image is updated."
