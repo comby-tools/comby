02fd8994b6a686baf9ba12a722dfaf784fa1af7a
Rijnard van Tonder <rvantonder@gmail.com>
2020-07-07 21:56:17 -0700

    update builds for 4.10

--- scripts/build-base-dependencies-alpine-image.sh
+++ scripts/build-base-dependencies-alpine-image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ..
-docker build --no-cache --tag comby/base-dependencies-alpine-3.10 -f dockerfiles/alpine/base-dependencies/Dockerfile .
-docker push comby/base-dependencies-alpine-3.10:latest
+docker build --no-cache --tag comby/base-dependencies-alpine-3.12 -f dockerfiles/alpine/base-dependencies/Dockerfile .
+docker push comby/base-dependencies-alpine-3.12:latest
 echo "Now run ./build-docker-binary-releases.sh to make sure the base dependencies image is updated."
