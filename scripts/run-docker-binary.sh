a092194dd37093cc7873d5bc142d3d4f319e1610
Rijnard van Tonder <rvantonder@gmail.com>
2020-07-07 22:07:01 -0700

    fix up docker release build with sqlite dep

--- scripts/run-docker-binary.sh
+++ scripts/run-docker-binary.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # mount /tmp/host to tmp in docker and run the binary
-docker run -it -v /tmp/host:/tmp comby-alpine-binary-build
+docker run -it -v /tmp/host:/tmp comby-alpine-binary-release
