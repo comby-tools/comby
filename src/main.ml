e0a07301f368e510a63a50e0d1effe413befdf6c
Rijnard van Tonder <rvantonder@gmail.com>
2020-07-22 14:09:02 -0700

    bump 0.17.0

--- src/main.ml
+++ src/main.ml
@@ -253,7 +253,7 @@ let parse_comby_dot_file () =
 
 let () =
   Scheduler.Daemon.check_entry_point ();
-  Command.run default_command ~version:"0.16.0" ~extend:(fun _ ->
+  Command.run default_command ~version:"0.17.0" ~extend:(fun _ ->
       match Sys.file_exists ".comby" with
       | `Yes -> parse_comby_dot_file ()
       | _ -> [])
