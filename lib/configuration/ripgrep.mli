e1f5c6835d8b859b559d6ce339f9bce525d21f4a
Rijnard van Tonder <rvantonder@gmail.com>
2020-08-12 17:50:27 -0700

    revert 872ca91ba5 for release

--- lib/configuration/ripgrep.mli
+++
@@ -1,6 +0,0 @@
-open Core
-
-(* [run pattern args] accepts a [pattern] and list of extra ripgrep-compatible
-   arguments [args], for example, ["-g"; "*.go"; "-g"; "*.ts"]. Returns a list
-   of files if the commands succeeds. *)
-val run : pattern:string -> args:string list -> string list Or_error.t
--- lib/configuration/specification.ml
+++ lib/configuration/specification.ml
@@ -1,5 +1,3 @@
-open Core
-
 open Language
 
 type t =
@@ -7,38 +5,6 @@ type t =
   ; rule : Rule.t option
   ; rewrite_template : string option
   }
-
-let escape s =
-  let rec aux chars =
-    match chars with
-    | [] -> []
-    | x :: xs ->
-      match x with
-      | '\\' | '.' | '+' | '*' | '?' | '(' | ')' | '|' | '[' | ']' | '{' | '}' | '^' | '$' as c ->
-        '\\' :: c :: (aux xs)
-      | c -> c :: (aux xs)
-  in
-  aux (String.to_list s)
-  |> String.of_char_list
-
-let to_regex { match_template; _ } =
-  let word_pattern = {|\w+|} in
-  let hole_patterns =
-    [ {|(:\[|} ^ word_pattern ^ {|\])|}
-    ; {|(:\[\[|} ^ word_pattern ^ {|\]\])|}
-    ; {|(:\[|} ^ word_pattern ^ {|\.\])|}
-    ; {|(:\[|} ^ word_pattern ^ {|\\n\])|}
-    ; {|(:\[|} ^ {|[ ]+(|} ^ word_pattern ^ {|)|} ^ {|\])|}
-    ]
-  in
-  let regexes = String.concat ~sep:"|" hole_patterns in
-  let spaces = Re2.create_exn {|\s+|} in
-  let m = Re2.split (Re2.create_exn regexes) match_template in
-  (* Escape regex metachars *)
-  let m = List.map m ~f:escape in
-  (* Replace contiguous spaces with the regex \s+ *)
-  let m = List.map m ~f:(fun split -> Re2.replace_exn ~f:(fun _ -> {|\s+|}) spaces split) in
-  Format.sprintf "(%s)" @@ String.concat m ~sep:")(?s:.)*?("
 
 let create ?rewrite_template ?rule ~match_template () =
   { match_template; rule; rewrite_template }
