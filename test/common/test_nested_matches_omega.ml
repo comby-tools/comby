a0f3d37854f2ed240775550e6351f379cb453da8
Rijnard van Tonder <rvantonder@gmail.com>
2021-02-06 21:22:44 -0700

    add nested rule test

--- test/common/test_nested_matches_omega.ml
+++ test/common/test_nested_matches_omega.ml
@@ -4,9 +4,22 @@ open Test_helpers
 
 include Test_omega
 
-let run ?(m = (module Generic : Matchers.Matcher)) ?(configuration = configuration) source match_template () =
+let run
+    ?(m = (module Generic : Matchers.Matcher))
+    ?(configuration = configuration)
+    ?rule
+    source
+    match_template
+    () =
   let (module M) = m in
-  M.all ~configuration ~nested:true ~template:match_template ~source ()
+  let nested =
+    match rule with
+    | None -> true
+    | Some rule ->
+      let options = Rule.create rule |> Or_error.ok_exn |> Rule.options in
+      options.nested
+  in
+  M.all ~configuration ~nested ~template:match_template ~source ()
   |> function
   | [] -> print_string "No matches."
   | matches ->
@@ -45,6 +58,17 @@ let%expect_test "nested_matches" =
   let source = {|a(b(c(d(e))))|} in
   let match_template = {|:[[f]](:[x])|} in
   run source match_template ();
+  [%expect_exact {|1:a(b(c(d(e))))
+1:b(c(d(e)))
+1:c(d(e))
+1:d(e)
+|}]
+
+let%expect_test "nested_matches_from_rule" =
+  let source = {|a(b(c(d(e))))|} in
+  let match_template = {|:[[f]](:[x])|} in
+  let rule = {|where nested|} in
+  run ~rule source match_template ();
   [%expect_exact {|1:a(b(c(d(e))))
 1:b(c(d(e)))
 1:c(d(e))
