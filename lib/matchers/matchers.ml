56517216e94aa7a0a513c20b4ee7e6296461efaf
Rijnard van Tonder <rvantonder@gmail.com>
2021-03-22 22:47:50 -0700

    more simplification

--- lib/matchers/matchers.ml
+++ lib/matchers/matchers.ml
@@ -1,12 +1,12 @@
 module Configuration = Configuration
 module Languages = Languages
 module Metasyntax = Metasyntax
-module Matcher = Types.Matcher
 
 module Alpha = Engine.Make(Alpha.Make)
 module Omega = Engine.Make(Omega.Make)
 
 module Engine = Types.Engine
+module Matcher = Types.Matcher
 module Syntax = Types.Syntax
 module Hole = Types.Hole
 module Language = Types.Language
--- lib/matchers/matchers.mli
+++ lib/matchers/matchers.mli
@@ -1,13 +1,12 @@
 module Configuration = Configuration
-module Languages : module type of Languages
-module Metasyntax : module type of Metasyntax
+module Languages = Languages
+module Metasyntax = Metasyntax
 
-module Matcher : module type of Types.Matcher
-module Engine : module type of Types.Engine
+module Alpha : Types.Engine.S
+module Omega : Types.Engine.S
 
-module Alpha : Engine.S
-module Omega : Engine.S
-
-module Syntax : module type of Types.Syntax
-module Hole : module type of Types.Hole
-module Language : module type of Types.Language
+module Engine = Types.Engine
+module Matcher = Types.Matcher
+module Syntax = Types.Syntax
+module Hole = Types.Hole
+module Language = Types.Language
