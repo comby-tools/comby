53b34afc8a6c847c6f523fa615bdd101f5f429eb
Gábor Lipták <gliptak@gmail.com>
2021-10-23 15:13:42 -0400

    Bump Alpine to 3.14 in Dockerfiles
    Signed-off-by: Gábor Lipták <gliptak@gmail.com>

--- scripts/build-base-dependencies-alpine-image.sh
+++ scripts/build-base-dependencies-alpine-image.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ..
-docker build --no-cache --tag comby/base-dependencies-alpine-3.12 -f dockerfiles/alpine/base-dependencies/Dockerfile .
+docker build --no-cache --tag comby/base-dependencies-alpine-3.14 -f dockerfiles/alpine/base-dependencies/Dockerfile .
 echo "If it succeeds, go and push the image:"
-echo "docker push comby/base-dependencies-alpine-3.12:latest"
+echo "docker push comby/base-dependencies-alpine-3.14:latest"
 echo "Now run ./build-docker-binary-releases.sh to make sure the base dependencies image is updated."
