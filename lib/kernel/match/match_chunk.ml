49378f64c772965d37440d2b646d645c82032348
Rijnard van Tonder <rvantonder@gmail.com>
2022-06-10 11:30:37 -0700

    cleanups

--- lib/kernel/match/match_chunk.ml
+++ lib/kernel/match/match_chunk.ml
@@ -1,6 +1,5 @@
 open Core_kernel
-
-type chunk_range = (Range.t * Range.t list)
+open Range
 
 type chunk_match =
   { content : string
@@ -9,49 +8,46 @@ type chunk_match =
   }
 [@@deriving yojson]
 
+let slice_source source { match_start = { offset = start; _ }; match_end = { offset = _end; _ } } =
+  let open Option in
+  let index f o = f source o '\n' in
+  let first_line = Option.value ~default:0 (index String.rindex_from start >>| (+) 1) in
+  let last_line = Option.value ~default:(String.length source) (index String.index_from _end) in
+  (first_line, String.slice source first_line last_line)
 
-let range_chunks source (ranges : chunk_range list) =
-  List.map ranges ~f:(fun (cover, ranges) ->
-      let first_line =
-        match String.rindex_from source cover.match_start.offset '\n' with
-        | None -> 0
-        | Some x -> x + 1
-      in
-      let last_line =
-        match String.index_from source cover.match_end.offset '\n' with
-        | None -> String.length source
-        | Some x -> x
-      in
-      { content = String.slice source first_line last_line
-      ; start =
-          { offset = first_line
-          ; line = cover.match_start.line
-          ; column = 1
-          }
-      ; ranges = List.rev ranges
-      })
+let to_range_chunk source (cover, ranges) =
+  let offset, content = slice_source source cover in
+  { content
+  ; start =
+      { offset
+      ; line = cover.match_start.line
+      ; column = 1
+      }
+  ; ranges
+  }
 
+let compare left right =
+  Int.compare left.match_start.offset right.match_start.offset
 
 let to_chunks source l =
-  let rec fold_matches acc (l: Range.t list) : chunk_range list =
-    match acc, l with
-    | _, [] -> acc
-    | [], (current :: tl) -> fold_matches [ (current, [ current ]) ] tl
-    | ((cover, ranges) :: tl as acc), (current :: rest) ->
-      if cover.match_end.line >= current.match_start.line then
-        let ranges = current :: ranges in
-        let cover =
-          if current.match_end.offset > cover.match_end.offset then
-            { cover with match_end = current.match_end }
-          else
-            cover
+  List.sort l ~compare
+  |> function
+  | [] -> []
+  | hd :: tl ->
+    List.fold ~init:[(hd, [hd])] tl ~f:(fun acc current ->
+        let cover, ranges, rest =
+          match acc with
+          | (cover, ranges) :: tl -> cover, ranges, tl
+          | _ -> assert false
         in
-        fold_matches ((cover, ranges) :: tl) rest
-      else
-        fold_matches ((current, [ current ]) :: acc) rest
-  in
-  l
-  |> List.sort ~compare:(fun (left : Range.t) right -> Int.compare left.match_start.offset right.match_start.offset)
-  |> fold_matches []
-  |> range_chunks source
-  |> List.rev
+        if cover.match_end.line >= current.match_start.line then
+          let cover =
+            if current.match_end.offset > cover.match_end.offset then
+              { cover with match_end = current.match_end }
+            else
+              cover
+          in
+          (cover, ranges @ [current]) :: rest
+        else
+          (current, [current]) :: acc)
+    |> List.rev_map ~f:(to_range_chunk source)
